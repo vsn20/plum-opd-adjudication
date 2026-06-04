5ccb59b6692b8db07088bf245c1b51f8b1056ca5
vsn20 <vuppalasainaman@gmail.com>
2026-06-05 00:29:09 +0530

    resolve CORS issues for Vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -63,7 +63,8 @@ app = FastAPI(
 # Allow requests from Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://plum-opd-adjudication.vercel.app",  # Your main Vercel domain
+        "https://plum-opd-adjudication-dk4eisjee-vsn20s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
